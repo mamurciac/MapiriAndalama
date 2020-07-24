8e250390c85b1296dbc28f3d9521f97b766e0fba
Michael Andrés Murcia <mamurciac@unal.edu.co>
2020-07-24 04:57:36 -0500

    App Restored (Database changed to Postgresql)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root :to => 'home#index' #This action indicates which view is shown when the app is run
-  #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
